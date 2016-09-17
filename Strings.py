5e2eb9359b1cfac143e4f2dd3e0d896aa83abb5d
Albert Lam <albert233@hotmail.com>
2016-09-17 22:18:43 +1000

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -4,3 +4,7 @@ string_list = [letter for letter in raw_input().strip()]
 index, character = raw_input().strip().split()
 string_list[int(index)] = character
 print ''.join(string_list)
+
+# Find a string
+string,substring = (raw_input().strip() for _ in range(2))
+[string[n:n+len(substring)] for n in range(0,len(string))].count(substring)
