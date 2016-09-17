dc6966f4a36170c953adbd3ca45484c55b688659
Albert Lam <albert233@hotmail.com>
2016-09-17 22:22:13 +1000

    Update Find the Second Largest Number.py

--- Find the Second Largest Number.py
+++ Find the Second Largest Number.py
@@ -1,4 +1,5 @@
 # Find the Second Largest Number
+## Find the second largest number in an integer array
 
 N, int_array = int(raw_input().strip()), map(int, raw_input().strip().split())
 print max([x for x in int_array if x != max(int_array)])
