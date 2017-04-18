5ad3519ce9d1966611effe3533a27a640c103b90
Albert Lam <albert233@hotmail.com>
2017-04-19 08:35:12 +1000

    Update reverseList.scala

--- scala/reverseList.scala
+++ scala/reverseList.scala
@@ -1,5 +1,5 @@
-# Reverse a List
-## You are given a list of N elements. Reverse the list without using the reverse function
+// Reverse a List
+// You are given a list of N elements. Reverse the list without using the reverse function
 
 def reverse[A](l: List[A]): List[A] = l match {
     case head :: tail => reverse(tail) ::: List(head)
