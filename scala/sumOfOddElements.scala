78a6a52b576f092121bd70b4032cb56abecbac8d
Albert Lam <albert233@hotmail.com>
2017-04-19 08:35:45 +1000

    Update sumOfOddElements.scala

--- scala/sumOfOddElements.scala
+++ scala/sumOfOddElements.scala
@@ -1,4 +1,4 @@
-# Sum of Odd Elements
-## Return the sum of odd elements from the given list
+// Sum of Odd Elements
+// Return the sum of odd elements from the given list
 
 def f(arr:List[Int]):Int = arr.filter(_ % 2 != 0).sum
