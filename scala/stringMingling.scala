e79f7fcd7b7380203628f03e82ab767259facc19
Albert Lam <albert233@hotmail.com>
2017-04-18 14:11:52 +1000

    Create stringMingling.scala

---
+++ scala/stringMingling.scala
@@ -0,0 +1,13 @@
+# String Mingling
+## You are given two strings P and Q, determine the mingled string R
+
+val p = readLine().toList
+val q = readLine().toList
+    
+def f(p: List[Char], q: List[Char]): List[Char] = {
+    if (p.isEmpty) List()
+    else (p.head :: q.head :: f(p.tail, q.tail))
+
+}
+
+println(f(p, q).mkString(""))
