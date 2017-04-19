58183be23ad1afb9f8e461037d6b9a483a68bc4d
Albert Lam <albert233@hotmail.com>
2017-04-20 08:19:24 +1000

    Create filterPositions.scala

---
+++ scala/filterPositions.scala
@@ -0,0 +1,6 @@
+// Filter Positions in a List
+// For a given list with N integers, return a new list removing the elements at odd positions
+
+def f(arr:List[Int]):List[Int] = {
+	arr.zipWithIndex.filter(_._2 % 2 == 1).map(_._1)
+}
