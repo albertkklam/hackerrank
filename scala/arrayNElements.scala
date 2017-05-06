c3e0a8e2c050e149fa37a6e6a3ef6c1dfaedcd31
Albert Lam <albert233@hotmail.com>
2017-05-07 01:57:10 +1000

    Update arrayNElements.scala

--- scala/arrayNElements.scala
+++ scala/arrayNElements.scala
@@ -1,7 +1,7 @@
 // Array of N Elements
 // Create an array of N integers, where the value of N is passed as an argument to the pre-filled function in your editor
 
-def f(num:Int) : List[Int] = {
+def f(num:Int): List[Int] = {
     val list = List.range(0,num)
     return list
 } 
