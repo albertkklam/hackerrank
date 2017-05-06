bc0eec8229aa6b40cbf9703463d6032397c928dd
Albert Lam <albert233@hotmail.com>
2017-05-07 01:57:31 +1000

    Update fibonacci.scala

--- scala/fibonacci.scala
+++ scala/fibonacci.scala
@@ -1,7 +1,7 @@
 // Fibonacci Numbers
 // Complete the Fibonacci function to return the Nth term. We start counting from Fibonacci(1) = 0. This might differ from some other notations that treats Fibonacci(0) = 0
 
-def fibonacci(x:Int):Int = x match {
+def fibonacci(x:Int): Int = x match {
     case 1 => 0
     case 2 => 1
     case _ => fibonacci(x-1) + fibonacci(x-2)
