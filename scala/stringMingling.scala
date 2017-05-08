12599343d24d0c39bca64c109f701f5433a8c07f
Albert Lam <albert233@hotmail.com>
2017-05-09 00:32:51 +1000

    Update stringMingling.scala

--- scala/stringMingling.scala
+++ scala/stringMingling.scala
@@ -1,13 +1,14 @@
 // String Mingling
 // You are given two strings P and Q, determine the mingled string R
 
-val p = readLine().toList
-val q = readLine().toList
-    
-def f(p: List[Char], q: List[Char]): List[Char] = {
-    if (p.isEmpty) List()
-    else (p.head :: q.head :: f(p.tail, q.tail))
-
+object Solution {
+    def main(args: Array[String]) {
+        val p = readLine().toList
+        val q = readLine().toList
+        def f(p: List[Char], q: List[Char]): List[Char] = p match {
+            case Nil => Nil
+            case _ => p.head :: q.head :: f(p.tail, q.tail)
+        }
+        println(f(p, q).mkString(""))
+    }
 }
-
-println(f(p, q).mkString(""))
