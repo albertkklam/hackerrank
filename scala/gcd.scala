cc0a23a17386360a60f81d51438cc827442bf715
Albert Lam <albert233@hotmail.com>
2017-05-10 00:29:53 +1000

    Update gcd.scala

--- scala/gcd.scala
+++ scala/gcd.scala
@@ -2,6 +2,6 @@
 // Given two integers x and y, a recursive technique to find their GCD is the Euclidean Algorithm
 
 def gcd(x: Int, y: Int): Int = y match {
-    case 0 => x;
-    case _=> gcd(y, x%y)
+    case 0 => x
+    case _ => gcd(y, x%y)
 }
