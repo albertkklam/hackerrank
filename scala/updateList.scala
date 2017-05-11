ece82a1ac20e6263ae6f171e32a3f42b5e109877
Albert Lam <albert233@hotmail.com>
2017-05-12 00:44:35 +1000

    Update updateList.scala

--- scala/updateList.scala
+++ scala/updateList.scala
@@ -1,4 +1,4 @@
 // Update List
 // Update the values of a list with their absolute values
 
-def f(arr:List[Int]):List[Int] = arr.map(x => x.abs)
+def f(arr: List[Int]): List[Int] = arr.map(x => x.abs)
