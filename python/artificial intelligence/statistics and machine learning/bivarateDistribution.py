47c02556fce8d2babda51344a0f6c73b8dcad8be
Albert Lam <albert233@hotmail.com>
2017-11-26 12:45:39 +1100

    Create bivarateDistribution.py

---
+++ python/artificial intelligence/statistics and machine learning/bivarateDistribution.py
@@ -0,0 +1,6 @@
+# Correlation and Regression Lines - A Quick Recap #4
+## https://www.hackerrank.com/challenges/correlation-and-regression-lines-4/problem
+
+y = 7
+x = 9.0*y/20 + 107.0/20
+print(x)
