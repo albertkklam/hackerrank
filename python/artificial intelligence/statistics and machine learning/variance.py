49eba511b99ada6252b37e4d5de9ece9b63c0516
Albert Lam <albert233@hotmail.com>
2017-11-26 22:26:48 +1100

    Create variance.py

---
+++ python/artificial intelligence/statistics and machine learning/variance.py
@@ -0,0 +1,8 @@
+# Correlation and Regression Lines - A Quick Recap #5
+## https://www.hackerrank.com/challenges/correlation-and-regression-lines-5/problem
+
+betaXY = 9/20
+betaYX = 4/5
+varX = 9
+varY = betaYX * varX / betaXY
+print(varY)
