a3b31f5d33c12aa49815a57a62492d1b237b46f0
Albert Lam <albert233@hotmail.com>
2018-06-16 13:28:49 +1000

    Create exceptions.py

---
+++ python/exceptions.py
@@ -0,0 +1,11 @@
+# Exceptions
+## https://www.hackerrank.com/challenges/exceptions/problem
+### Python 3.6
+
+t = int(input())
+for _ in range(t):
+    a, b = input().split(' ')
+    try:
+        print(int(a) // int(b))
+    except (ZeroDivisionError, ValueError) as e:
+        print("Error Code:", e)
