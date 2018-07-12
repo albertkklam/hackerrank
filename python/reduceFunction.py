181360ccac28fd58b37e549ce4d294935cca8ebc
Albert Lam <albert233@hotmail.com>
2018-07-12 21:41:54 +1000

    Create reduceFunction.py

---
+++ python/reduceFunction.py
@@ -0,0 +1,17 @@
+# Reduce Function
+## https://www.hackerrank.com/challenges/reduce-function/problem
+### Python 3.6
+
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = reduce(lambda x, y: x * y, fracs)
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
