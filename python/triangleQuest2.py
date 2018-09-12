38a6cccd39362634820eb37eaa392373d1250090
Albert Lam <albert233@hotmail.com>
2018-09-12 10:12:02 -0500

    Update triangleQuest2.py

--- python/triangleQuest2.py
+++ python/triangleQuest2.py
@@ -1,5 +1,6 @@
 # Triangle Quest 2
-## Given a positive integer N, print a palindromic triangle of size N
+## https://www.hackerrank.com/challenges/triangle-quest-2/problem
+### Python 2.7
 
 for i in range(1,int(raw_input())+1):
     print sum(map(lambda x: 10**x,range(0,i)))**2
