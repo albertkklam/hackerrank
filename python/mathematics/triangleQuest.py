df55c8175030bbdc34d7acbc3b6a43de7794518c
Albert Lam <albert233@hotmail.com>
2018-09-12 10:11:19 -0500

    Update and rename python/triangleQuest.py to python/mathematics/triangleQuest.py

--- python/mathematics/triangleQuest.py
+++ python/mathematics/triangleQuest.py
@@ -1,5 +1,6 @@
 # Triangle Quest
-##  Print a numerical triangle of height N-1
+## https://www.hackerrank.com/challenges/python-quest-1/problem
+### Python 2.7
 
 for i in range(1,input()):
     print sum(map(lambda x: 10**x,range(0,i)))*i
