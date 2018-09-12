0e7dbe3c229b35777822756e1b2bbe1fbc9256e0
Albert Lam <albert233@hotmail.com>
2018-09-12 10:07:51 -0500

    Update and rename python/findAString.py to python/strings/findAString.py

--- python/strings/findAString.py
+++ python/strings/findAString.py
@@ -1,5 +1,6 @@
 # Find a string
-## Find the number of times a substring appears in a string
+## https://www.hackerrank.com/challenges/find-a-string/problem
+### Python 2.7
 
 string,substring = (raw_input().strip() for _ in range(2))
 [string[n:n+len(substring)] for n in range(0,len(string))].count(substring)
